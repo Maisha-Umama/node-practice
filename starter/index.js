3197d2c7b9886f039c0a88b0c30145b0d592fe9f
Maisha Umama <umamamaisha@gmail.com>
2021-02-09 14:07:32 +0600

    learning routing

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 //blocking synchronus way
 //const textIn = fs.readFileSync('./txt/./input.txt', 'utf-8') ;
@@ -10,16 +12,36 @@ const fs = require('fs');
 
 
 //non-blocking asynchronus way
-fs.readFile('./txt/./startttttttt.txt', 'utf-8', (err, data1)=>{
- if(err) return console.log('ERROR!!!!');
-    fs.readFile(`./txt/${data1}.txt` , 'utf-8' ,(err,data2)=>{
-     console.log(data2);
-     fs.readFile(`./txt/$append.txt` , 'utf-8' ,(err,data3)=>{
-      console.log(data3);
+//fs.readFile('./txt/./startttttttt.txt', 'utf-8', (err, data1)=>{
+// if(err) return console.log('ERROR!!!!');
+//    fs.readFile(`./txt/${data1}.txt` , 'utf-8' ,(err,data2)=>{
+//     console.log(data2);
+//      fs.readFile(`./txt/$append.txt` , 'utf-8' ,(err,data3)=>{
+//       console.log(data3);
 
-      fs.writeFile(`./txt/$final.txt`, `${data2}\n${data3}`, 'utf-8' ,err =>{
-        console.log("YOUR FILE IS WRIITEN");
-           });
-        });
-     });
+//        fs.writeFile(`./txt/$final.txt`, `${data2}\n${data3}`, 'utf-8' ,err =>{
+//          console.log("YOUR FILE IS WRIITEN");
+//       });
+//     });
+//   });
+//});
+
+const server = http.createServer((req,res)=>{
+   const pathName = req.url;
+
+   if(pathName === '/' || pathName === '/overview'){
+       res.end(" THIS IS THE OVERVIEW");
+   }
+   else if(pathName === "/product"){
+       res.end("THIS IS THE PRODUCT");
+   }
+   else{ 
+       res.writeHead(404);
+       
+       res.end('<h1>PAGE NOT FOUND!!!<h1>');
+   }
 });
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log("LISTENING TO THE PORT 8000")
+})
